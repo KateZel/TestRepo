be023cc3cb604fc584f3ceecf958df00c0bf6463
KateZel <zelentsova.ekaterina@gmail.com>
2022-08-22 10:52:33 +0300

    added same..

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -6,3 +6,8 @@ else
     echo -e "$dir is not directory!\n"
     exit 101
 fi
+echo -e "Checking disk usage in $dir\n"
+
+top1=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | cut -f 2)
+disk=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | cut -f 1)
+
